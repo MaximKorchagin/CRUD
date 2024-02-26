0c2c304ea5c0644e6c874a134caa38e7da1ff997
Maxim.Korchagin <Airfallsc2@gmail.com>
2024-02-26 08:19:29 +0300

    2.2

---
+++ src/main/java/org/example/JavaConfig.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepositoryStubImpl();
+    }
+}
